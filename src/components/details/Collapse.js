79aa9e9e7c30b422c29c0a008b9cf8745dbbe9fd
Golamrabbani-ux <golamrabbani227417@gmail.com>
2022-02-22 16:09:04 +0600

    update

---
+++ src/components/details/Collapse.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Collapse = () => {
+  return (
+    <div id="accordion-collapse" data-accordion="collapse">
+      <h2 id="accordion-collapse-heading-1">
+        <button
+          type="button"
+          className="flex items-center focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 justify-between p-5 w-full font-medium text-left border border-gray-200 dark:border-gray-700 border-b-0 text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-t-xl"
+          data-accordion-target="#accordion-collapse-body-1"
+          aria-expanded="true"
+          aria-controls="accordion-collapse-body-1"
+        >
+          <span>What is Flowbite?</span>
+          <svg
+            data-accordion-icon
+            className="w-6 h-6 shrink-0 rotate-180"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </h2>
+    </div>
+  );
+};
+
+export default Collapse;
--- src/components/details/index.js
+++ src/components/details/index.js
@@ -20,7 +20,7 @@ const Details = () => {
   return (
     <div className="grid grid-cols-1 gap-10 my-12 lg:grid-cols-2">
       <div className="">
-        <div className="flex justify-center">
+        <div className="flex justify-center mb-12">
           <img src={image} alt={title} className="lg:h-96 xl:h-100" />
         </div>
         <div className="mt-3 grid grid-cols-3 gap-x-3">
@@ -38,9 +38,10 @@ const Details = () => {
           gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis.
           Nullam sit amet enim.
         </p>
-        <h3 className="text-3xl font-bold text-gray-700 mb-2 lg:mb-6">
-          ${price}
-        </h3>
+        <div className="mb-2 flex items-center gap-x-5 lg:mb-6">
+          <h3 className="text-lg line-through">${price}</h3>
+          <h3 className="text-4xl font-bold">${price}</h3>
+        </div>
         <Size />
         <div className="my-6 flex flex-col gap-3 md:my-10 md:flex-row lg:mb-6">
           <button className="px-10 py-2.5 text-red-700 font-semibold border border-red-700 outline-none shadow-lg transition hover:bg-red-700 hover:text-white">
